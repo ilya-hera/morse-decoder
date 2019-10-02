8a74138d88536ca09b694aed717bf6a246271ad4
Ilya <ilya_gerasimchik@mail.ru>
2019-10-02 15:43:46 +0300

    task is done

--- src/index.js
+++ src/index.js
@@ -38,7 +38,46 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let userMessage = '';
+    let position = 0;
+    while (position < expr.length){
+
+        if (expr[position] == '*') {
+            userMessage += ' ';
+            position += 10;
+            continue;
+        }
+
+        const currentLetter = expr.slice(position, position + 10);
+        let morseCode = '';
+
+       
+        for (let j = 0; j < currentLetter.length; j++){
+            if ((currentLetter[j] == '0') && (j % 2 == 0)) {
+                j++;
+            } else if ((currentLetter[j] == '1') && (currentLetter[j + 1] == '1')) {
+                morseCode += '-';
+                j++;
+            } else {
+                morseCode += '.';
+                j++
+            }
+        }
+
+
+        for (let [code, symbol] of Object.entries(MORSE_TABLE)) {
+            if (code == morseCode) {
+                userMessage += symbol;
+                break;
+            }
+        }
+
+
+        position += 10;
+    }
+
+
+    return userMessage;
 }
 
 module.exports = {
